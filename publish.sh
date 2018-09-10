6138ef15cc5693d97574a79117fe078635585ce2
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2018-09-10 11:33:36 +0200

    Add publish script.
    Add publish script as described on Hugo docs pages:
    https://gohugo.io/hosting-and-deployment/hosting-on-github/#put-it-into-a-script-1

---
+++ publish.sh
@@ -0,0 +1,32 @@
+remote="origin"
+branch="master"
+dir="public"
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication."
+rm -rf $dir
+mkdir $dir
+git worktree prune
+rm -rf .git/worktrees/$dir/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B $branch $dir $remote/$branch
+
+# echo "Removing existing files"
+# rm -rf $dir/*
+
+echo "Generating site."
+hugo
+
+echo "Updating gh-pages branch"
+if [ $# -eq 0 ] then
+	date=`date '+%Y-%m-%d %H:%M:%S'`
+	msg="Add generated site as of $date."
+else
+	msg=$0
+fi
+cd $dir && git add --all && git commit -m "$msg"
