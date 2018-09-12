9a85231187e970eb48dd1a3f05ec3c401f6945a7
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2018-09-12 09:57:30 +0200

    Make menu toggle buttons use openNav and closeNav functions directly.

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,25 +1,17 @@
-function openNav($nav, $body) {
+var $body = $('body')
+var $nav = $("#mySidenav");
+
+function openNav() {
     $nav.width(250);
     $body.css("background-color", "rgba(0,0,0,0.4)");
 }
 
-function closeNav($nav, $body) {
+function closeNav() {
     $nav.width(0);
     $body.css("background-color", "white");
 } 
 
-function toggleNav() {
-    var $nav = $("#mySidenav");
-    var $body = $("body")
-    if ($nav.width()) {
-        closeNav($nav, $body);
-    } else {
-        openNav($nav, $body);
-    }
-}
-
 var $toggle = $('.menu-toggle, .closebtn');
-var $body = $('body')
 
 function addMenuListener() {
     $toggle.click(function() {
